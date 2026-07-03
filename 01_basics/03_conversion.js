af8d3e8983eeb8e504ed3cfcdabd59e838dfdd43
harshitac-12 <Harshitach35@gmail.com>
2026-07-03 20:53:20 +0000

    conversions of datatypes

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,22 @@
+let score = "66abs";
+
+console.log(typeof score); //int if "33" then string
+
+let valueInNumber = Number(score); //converts in Number with capital N
+console.log(typeof valueInNumber); //number
+console.log(valueToNumber); //NaN (not a number)
+/* here type comes out number but the value is not a number */ 
+// "33"=>33
+//"33abc"=>NaN
+//true=>1; false=>0;
+//null=>0;
+let isLoggedIn = 1;
+
+let booleanIsLoggedIn = Boolean(isLoggedIn); //true
+console.log(booleanIsLoggedIn);
+//"harsh" =>true
+//" " => false
+ let someNumber = String(someNumber)
+ console.log(someNumber); //33
+ console.log(typeof someNumber); //string
+ 

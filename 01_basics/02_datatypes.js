f36fc68e00732747e7f0c7d5382c8d9bbafc76c7
harshitac-12 <Harshitach35@gmail.com>
2026-07-03 20:25:43 +0000

    datatypesss

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; //there were a older version of js which do not have classes,modules etc 
+            //,new version have so strict treat all js code as newer versions
+//alert( 3 + 3) //we are using node js not browser
+
+
+console.log(3+
+    3) //code readibility should be high
+
+
+    //documentation:Mdn (for us)
+    //standard documentation is ECMA which includes standards in js like how to write code in js
+ 
+
+    //datatypes
+    let name="harsh" //string " "
+    let age=18  //int
+    let isLogged=false //bool T/F
+
+    //big int ,used in big companies
+    //null=> standalone value (representation of empty value) ,value is assigned as null(empty)
+    //undefined=>value not assigned
+    //symbol=> unique
+
+    //object
+
+    console.log(typeof age); //int
+    console.log(typeof null); //null is object
+    console.log(typeof undefined) //undefined

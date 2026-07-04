581fe3acff083b4bf9eefc893ed354c25d845d3f
harshitac-12 <Harshitach35@gmail.com>
2026-07-04 10:58:45 +0000

    memory

---
+++ 01_basics/07_memory.js
@@ -0,0 +1,27 @@
+// heap(reference/address) non primitive
+// stack(copy) non primitive
+
+let myYoutubeName = "harshitttaa"
+
+let anotherName = myYoutubeName 
+anothername = "billionsMoney";
+
+console.log(myYoutubeName);
+console.log(anothername);//value di gyi thi
+
+let userOne = {
+    email : "user@google.com",
+    upi : "ser@ybl" 
+}
+
+let userTwo = userOne 
+
+userTwo.email = "harshita@gmail.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);// as reference passed both show same output
+
+
+
+ 
+

51c455a69d08726f98e73736bdd7d828399cd072
harshitac-12 <Harshitach35@gmail.com>
2026-07-09 21:51:16 +0000

    pending

---
+++ 03_basics/04_arrow.js
@@ -0,0 +1,65 @@
+/*This code introduces one of the most important JavaScript topics:
+
+this keyword
+Arrow functions
+Regular functions vs arrow functions
+Implicit and explicit return
+Returning objects from arrow functions */
+
+
+//this refers to the object that called the method.
+
+const user = {
+    username: "hitesh",
+    price: 999,
+
+    welcomeMessage: function() {
+        console.log(`${this.username} , welcome to website`);
+        console.log(this);
+    }
+
+}
+
+// user.welcomeMessage()
+user.username = "sam"  //changing objects
+ user.welcomeMessage()
+
+// console.log(this);
+
+// function chai(){
+//     let username = "hitesh"
+    console.log(this.username); //always refers to the current value of the object's property.
+// }
+
+// chai()
+
+// const chai = function () {
+//     let username = "hitesh"
+//     console.log(this.username);
+// }
+
+const chai =  () => {
+    let username = "hitesh"
+    console.log(this);
+}
+
+
+// chai()
+
+// const addTwo = (num1, num2) => {
+//     return num1 + num2
+// }
+
+// const addTwo = (num1, num2) =>  num1 + num2
+
+// const addTwo = (num1, num2) => ( num1 + num2 )
+
+const addTwo = (num1, num2) => ({username: "hitesh"})
+
+
+console.log(addTwo(3, 4))
+
+
+// const myArray = [2, 5, 3, 7, 8]
+
+// myArray.forEach()
